e8dfa17758e56ec9e8934097c7d2ccbd1f645736
James Kong <fishkingsin@gmail.com>
2021-11-29 22:24:28 +0800

    WIP

--- deploy.js
+++ deploy.js
@@ -1,15 +1,17 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require('web3');
 
 // Getting the output of our compiled Solidity Contract
 const contractFile = require('./compile');// Initialization
 const bytecode = contractFile.evm.bytecode.object;
 const abi = contractFile.abi;
-console.log("abi : " + abi);
-const provider = new HDWalletProvider(
-    process.env.REACT_APP_METAMASK_CODE,
-    process.env.REACT_APP_INFURA_URL
-);
+// console.log("abi "+ JSON.stringify(abi, "", 2));
+const provider = new HDWalletProvider({
+    mnemonic: {
+        phrase: process.env.REACT_APP_METAMASK_CODE,
+    },
+    providerOrUrl: process.env.REACT_APP_INFURA_URL
+});
 
 const web3 = new Web3(provider);
 
@@ -20,11 +22,8 @@ const deploy = async () => {
     console.log('Attempting to deploy from account', accounts[0]);
 
     // deploying our contract
-    const result = await new web3.eth.Contract(JSON.parse(abi))
-        .deploy({ data: bytecode })
-        .send({ gas: '1000000', from: accounts[0] });
+    const result = await new web3.eth.Contract(abi, accounts[0], { gas: '1000000', from: accounts[0] });
 
-    
     console.log('Contract deployed to', result.options.address);
 };
 deploy();
